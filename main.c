82f3ac2431ee70a66677df1bcbd9601d5b3dc1ad
Gregory Soares <gregory16704@gmail.com>
2024-09-07 17:56:52 -0300

    Revert "Revert "Produto final""
    This reverts commit fdce08ba8b0646840c958969d6e0defcacb8f745.

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdint.h>  // Para tipos de dados de tamanho fixo
+#include <stdlib.h>  // Para a função exit()
+#include <locale.h>
+#include <assert.h>
+
+#include "lib.h"
+#include "func.h"
+
+#define MEMORY_SIZE 1024
+
+
+uint16_t memory[MEMORY_SIZE];
+void *memory_pointer = memory;
+
+int main (int argc, char **argv){
+	if (argc != 2) {
+		printf("usage: %s [bin_name]\n", argv[0]);
+		exit(1);
+	}
+    int g;
+    load_binary_to_memory(argv[1], memory_pointer,MEMORY_SIZE);
+    for(g = 0; g < MEMORY_SIZE; g++){
+        if(g == 10){
+            break;
+        }
+        printf("memory[%d]: %d\n",g,memory[g]);
+    }
+
+    int cond = 1;
+    Instruction comando;
+    int i = 0;
+    while(cond){
+            comando.linha = i;
+            int format = extract_bits(memory[i], 15,1);
+            printf("format: %d, ",format);
+            if(format == 0){
+                comando.opcode = extract_bits(memory[i], 9, 6);
+                comando.reg_dst = extract_bits(memory[i], 6, 3);
+                comando.reg_1 = extract_bits(memory[i], 3, 3);
+                comando.reg_2 = extract_bits(memory[i], 0, 3);
+                printf("opcode: %d, reg_dst: %d, reg_1: %d, reg_2: %d\n",comando.opcode,comando.reg_dst,comando.reg_1,comando.reg_2);
+                execute_instructionr(comando);
+            }
+            else {
+                comando.opcode = extract_bits(memory[i], 13, 2);
+                comando.reg_dst = extract_bits(memory[i], 10, 3);
+                comando.imediato = extract_bits(memory[i], 0, 10);
+                printf("opcode: %d, reg_dst: %d, imediato: %d\n",comando.opcode,comando.reg_dst,comando.imediato);
+                execute_instructioni(&comando);
+                i = comando.linha;       
+                }
+            i++;
+        }   
+	return 0;
+}
