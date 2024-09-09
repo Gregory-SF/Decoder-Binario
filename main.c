651a149d8c7412ad97dff5ab2cb041590b972898
Gregory Soares <gregory16704@gmail.com>
2024-09-09 14:44:20 -0300

    Arrumando laod e store, e começando syscall

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>  // Para tipos de dados de tamanho fixo
-#include <stdlib.h>  // Para a função exit()
+#include <stdlib.h>  // Para exit()
 #include <locale.h>
 #include <assert.h>
 
 #include "lib.h"
 #include "func.h"
-
-#define MEMORY_SIZE 1024
-
 
 uint16_t memory[MEMORY_SIZE];
 void *memory_pointer = memory;
@@ -28,10 +25,11 @@ int main (int argc, char **argv){
     }
     int cond = 1;
     Instruction comando;
-    int i = 0;
+    int i = 1;
     while(cond){
             comando.linha = i;
             int format = extract_bits(memory[i], 15,1);
+            printf("Linha %d\n",i);
             printf("format: %d, ",format);
             if(format == 0){
                 comando.opcode = extract_bits(memory[i], 9, 6);
@@ -39,17 +37,17 @@ int main (int argc, char **argv){
                 comando.reg_1 = extract_bits(memory[i], 3, 3);
                 comando.reg_2 = extract_bits(memory[i], 0, 3);
                 printf("opcode: %d, reg_dst: %d, reg_1: %d, reg_2: %d\n",comando.opcode,comando.reg_dst,comando.reg_1,comando.reg_2);
-                execute_instructionr(comando);
+                instruction_r(comando);
             }
             else {
                 comando.opcode = extract_bits(memory[i], 13, 2);
                 comando.reg_dst = extract_bits(memory[i], 10, 3);
                 comando.imediato = extract_bits(memory[i], 0, 10);
                 printf("opcode: %d, reg_dst: %d, imediato: %d\n",comando.opcode,comando.reg_dst,comando.imediato);
-                execute_instructioni(&comando);
+                instruction_i(&comando);
                 i = comando.linha;       
                 }
+            printf("--------------------\n");
             i++;
         }   
-	return 0;
 }
